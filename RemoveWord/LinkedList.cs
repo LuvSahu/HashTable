c11dbed6fa072416e6be83f953051d3660abda24
Luv sahu <lksahu404@gmail.com>
2022-06-25 17:22:17 +0530

    This code remove word in HashMap

---
+++ RemoveWord/LinkedList.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class LinkedList<K, V> where K : IComparable
+    {
+        public MyMapNode<K, V> Head;
+        public MyMapNode<K, V> Tail;
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        internal MyMapNode<K, V> Search(K key)
+        {
+            MyMapNode<K, V> temp = Head;
+            while (temp != null)
+            {
+                if (temp.key.Equals(key))
+                    return temp;
+                temp = temp.next;
+            }
+            return null;
+        }
+
+        public void Append(MyMapNode<K, V> node)
+        {
+            if (Head == null && Tail == null)
+            {
+                Head = node;
+                Tail = node;
+            }
+            else
+            {
+                Tail.next = node;
+                Tail = node;
+            }
+        }
+
+
+
+        public bool IsEmpty()
+        {
+            return Head == null && Tail == null;
+        }
+
+        public MyMapNode<K, V> Pop()
+        {
+
+            MyMapNode<K, V> temp = Head;
+            if (Head != null)
+            {
+
+                Head = Head.next;
+            }
+            return temp;
+        }
+
+        public MyMapNode<K, V> PopLast()
+        {
+            MyMapNode<K, V> tailTemp = Tail;
+            if (!IsEmpty())
+            {
+
+                MyMapNode<K, V> temp = Head;
+                while (temp.next != Tail)
+                {
+
+                    temp = temp.next;
+                }
+
+                temp.next = null;
+                Tail = temp;
+            }
+            return tailTemp;
+        }
+
+        public bool DeleteNode(MyMapNode<K, V> DeleteNode)
+        {
+
+            MyMapNode<K, V> temp = Head;
+            if (!IsEmpty())
+            {
+
+                if (DeleteNode.key.Equals(Head.key))
+                {
+                    Pop();
+                    return true;
+                }
+
+                if (DeleteNode.key.Equals(Tail.key))
+                {
+                    PopLast();
+                    return true;
+                }
+                while (temp != null)
+                {
+
+                    if (temp.next != null && temp.next.key.Equals(DeleteNode.key))
+                    {
+                        temp.next = DeleteNode.next;
+                        return true;
+                    }
+                    temp = temp.next;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ RemoveWord/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HashTable
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("We find frequncy from hashmap !");
+            LinkedHashMap<string, int> LinkedHashMap = new LinkedHashMap<string, int>(5);
+            string sentence = "to be or not to be";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach (string word in words)
+            {
+                int value = LinkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                LinkedHashMap.Add(word, value);
+
+            }
+            int frequency = LinkedHashMap.Get("not");
+            Console.WriteLine(frequency);
+
+            string Paragraph = "“Paranoids are not paranoid because they are " +
+                               "paranoid but because they keep putting themselves deliberately into" +
+                               " paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
+
+            foreach (string word in letters)
+            {
+                int value = LinkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                LinkedHashMap.Add(word, value);
+            }
+            int frequency1 = LinkedHashMap.Get("paranoid");
+            Console.WriteLine(frequency1);
+
+            LinkedHashMap.Remove("avoidable");
+            int frequency2 = LinkedHashMap.Get("avoidable");
+
+            Console.WriteLine(frequency2);
+
+        }
+
+    }
+}
